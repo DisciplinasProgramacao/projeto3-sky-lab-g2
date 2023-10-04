5c552cb0b847180eec76bee598c02b511c5266b6
Paula de Freitas <113747605+pauladefreitas@users.noreply.github.com>
2023-10-04 11:57:38 -0300

    add classe cliente v inicial

--- codigo/Cliente.java
+++ codigo/Cliente.java
@@ -1,38 +1,66 @@
-
-
 public class Cliente {
 
-	private String nome;
-	private String id;
-	private Veiculo[] veiculos;
+    private String nome;
+    private String id;
+    private Veiculo[] veiculos;
+    private int numVeiculos;
 
-	public Cliente(String nome, String id) {
-		this.nome = nome; 
-		this.id = id;
-	}
+    public Cliente(String nome, String id) {
+        this.nome = nome;
+        this.id = id;
+        this.veiculos = new Veiculo[10];
+        this.numVeiculos = 0;
+    }
 
-	public void addVeiculo(Veiculo veiculo) {
-		
-	}
+    public void addVeiculo(Veiculo veiculo) {
+        if (numVeiculos < veiculos.length) {
+            veiculos[numVeiculos] = veiculo;
+            numVeiculos++;
+        } else {
+            System.out.println("Limite de veículos atingido para este cliente.");
+        }
+    }
 
-	public Veiculo possuiVeiculo(String placa) {
-		
-	}
+    public Veiculo possuiVeiculo(String placa) {
+        for (int i = 0; i < numVeiculos; i++) {
+            if (veiculos[i].getPlaca().equals(placa)) {
+                return veiculos[i];
+            }
+        }
+        return null;
+    }
 
-	public int totalDeUsos() {
-		
-	}
+    public int totalDeUsos() {
+        int totalUsos = 0;
+        for (int i = 0; i < numVeiculos; i++) {
+            totalUsos += veiculos[i].totalDeUsos();
+        }
+        return totalUsos;
+    }
 
-	public double arrecadadoPorVeiculo(String placa) {
-		
-	}
+    public double arrecadadoPorVeiculo(String placa) {
+        Veiculo veiculo = possuiVeiculo(placa);
+        if (veiculo != null) {
+            
+            return veiculo.totalArrecadado();
+        } else {
+            return 0.0;  
+        }
+    }
 
-	public double arrecadadoTotal() {
-		
-	}
+    public double arrecadadoTotal() {
+        double totalArrecadado = 0.0;
+        for (int i = 0; i < numVeiculos; i++) {
+            totalArrecadado += veiculos[i].totalArrecadado();
+        }
+        return totalArrecadado;
+    }
 
-	public double arrecadadoNoMes(int mes) {
-		
-	}
-
+    public double arrecadadoNoMes(int mes) {
+        double arrecadadoMes = 0.0;
+        for (int i = 0; i < numVeiculos; i++) {
+            arrecadadoMes += veiculos[i].arrecadadoNoMes(mes);
+        }
+        return arrecadadoMes;
+    }
 }
