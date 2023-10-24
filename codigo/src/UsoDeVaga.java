acd3c903612fde08aef436dd027a0e7d21536f71
Paula de Freitas <113747605+pauladefreitas@users.noreply.github.com>
2023-10-24 14:53:23 -0300

    add documentacao as classes

--- codigo/src/UsoDeVaga.java
+++ codigo/src/UsoDeVaga.java
@@ -2,6 +2,9 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.EnumSet;
 
+/**
+ * Classe que representa o uso de uma vaga de estacionamento por um cliente.
+ */
 public class UsoDeVaga {
 
     private static final double FRACAO_USO = 0.25;
@@ -14,43 +17,77 @@ public class UsoDeVaga {
     private double valorPago;
     private EnumSet<Servico> servicosContratados;
 
-    public LocalDateTime getEntrada() {
-        return entrada;
-    }
-
-    public Vaga getVaga() {
-        return this.vaga;
-    }
-    
+    /**
+     * Construtor que cria uma instância de UsoDeVaga associada a uma vaga.
+     *
+     * @param vaga A vaga utilizada no uso.
+     */
     public UsoDeVaga(Vaga vaga) {
         this.vaga = vaga;
         this.valorPago = 0.0;
         this.servicosContratados = EnumSet.noneOf(Servico.class);
     }
 
+    /**
+     * Obtém a data e hora de entrada na vaga.
+     *
+     * @return A data e hora de entrada.
+     */
+    public LocalDateTime getEntrada() {
+        return entrada;
+    }
+
+    /**
+     * Obtém a vaga associada ao uso.
+     *
+     * @return A vaga associada.
+     */
+    public Vaga getVaga() {
+        return this.vaga;
+    }
+
+    /**
+     * Registra o uso da vaga com a data e hora de entrada especificadas.
+     *
+     * @param vaga    A vaga utilizada.
+     * @param entrada A data e hora de entrada na vaga.
+     */
     public void usarVaga(Vaga vaga, LocalDateTime entrada) {
         if (this.entrada == null) {
             this.entrada = entrada;
             vaga.disponivel();
-        } else {
-
         }
     }
 
+    /**
+     * Registra a saída do veículo da vaga com a data e hora de saída especificadas.
+     *
+     * @param saida A data e hora de saída da vaga.
+     */
     public void sair(LocalDateTime saida) {
         if (entrada != null) {
             this.saida = saida;
             valorPago();
             vaga.disponivel();
         } else {
-            //garantir fim da condição
+            // Condição não especificada
         }
     }
 
+    /**
+     * Contrata um serviço adicional para o uso da vaga.
+     *
+     * @param servico O serviço a ser contratado.
+     */
     public void contratarServico(Servico servico) {
         servicosContratados.add(servico);
     }
 
+    /**
+     * Calcula o valor a ser pago pelo uso da vaga.
+     *
+     * @return O valor a ser pago.
+     */
     public double valorPago() {
         if (entrada != null && saida != null) {
             Duration duracao = Duration.between(entrada, saida);
@@ -76,8 +113,7 @@ public class UsoDeVaga {
 
             return valorPago;
         } else {
-            
-            return 4.0; //TAXA MÍNIMA
+            return 4.0; // Taxa mínima
         }
     }
 }
