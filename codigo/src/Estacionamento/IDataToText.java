2db976c9a6241c48898d2779920e3407a21836aa
AlongiMaster <82001555+AlongiMaster@users.noreply.github.com>
2023-11-08 11:50:24 -0300

    IDataToText.java

--- codigo/src/Estacionamento/IDataToText.java
+++ codigo/src/Estacionamento/IDataToText.java
@@ -1,3 +1,8 @@
 public interface IDataToText {
+    /**
+     * Este método converte os dados internos de um objeto para uma representação textual.
+     * 
+     * @return Uma string contendo a representação textual dos dados do objeto.
+     */
     public String dataToText();
 }
