4cda05b43a11aeece3ff46496a59e6bdb85073b5
Paula de Freitas <113747605+pauladefreitas@users.noreply.github.com>
2023-11-21 17:20:57 -0300

    deletando coisas erradas

--- codigo/src/Estacionamento/IDataToText.java
+++
@@ -1,8 +0,0 @@
-public interface IDataToText {
-    /**
-     * Este método converte os dados internos de um objeto para uma representação textual.
-     * 
-     * @return Uma string contendo a representação textual dos dados do objeto.
-     */
-    public String dataToText();
-}
