06107d2d46f6e6a586e041f82d3037e552624e49
Wanessa Dias <130778028+diascw07@users.noreply.github.com>
2023-11-22 10:56:23 -0300

    UsoDeVaga.java

--- codigo/UsoDeVaga.java
+++ codigo/UsoDeVaga.java
@@ -1,25 +1,40 @@
+import java.time.LocalDateTime;
 
-
+// A classe UsoDeVaga representa o uso de uma vaga de estacionamento por um veículo.
 public class UsoDeVaga {
 
-	private static final double FRACAO_USO = 0.25;
-	private static final double VALOR_FRACAO = 4.0;
-	private static final double VALOR_MAXIMO = 50.0;
-	private Vaga vaga;
-	private LocalDateTime entrada;
-	private LocalDateTime saida;
-	private double valorPago;
+    // Constantes para cálculo do valor de pagamento.
+    private static final double FRACAO_USO = 0.25;
+    private static final double VALOR_FRACAO = 4.0;
+    private static final double VALOR_MAXIMO = 50.0;
 
-	public UsoDeVaga(Vaga vaga) {
-		
-	}
+    // A vaga associada ao uso.
+    private Vaga vaga;
 
-	public double sair() {
-		
-	}
+    // Data e hora de entrada do veículo na vaga.
+    private LocalDateTime entrada;
 
-	public double valorPago() {
-		
-	}
+    // Data e hora de saída do veículo da vaga.
+    private LocalDateTime saida;
 
+    // Valor pago pelo uso da vaga.
+    private double valorPago;
+
+    // Construtor da classe UsoDeVaga.
+    public UsoDeVaga(Vaga vaga) {
+        this.vaga = vaga;
+        this.entrada = LocalDateTime.now(); // Define a entrada como o momento atual.
+        this.saida = null; // A saída ainda não ocorreu.
+        this.valorPago = 0.0; // Inicializa o valor pago como 0.
+    }
+
+    // Registra a saída do veículo da vaga e calcula o valor a ser pago.
+    public double sair() {
+        // Implementação do método.
+    }
+
+    // Retorna o valor pago pelo uso da vaga.
+    public double valorPago() {
+        // Implementação do método.
+    }
 }
