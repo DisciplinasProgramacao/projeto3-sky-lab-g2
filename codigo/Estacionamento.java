f867ec39919724b4b79691a35be5384cc8bed5d4
Wanessa Dias <130778028+diascw07@users.noreply.github.com>
2023-11-22 10:55:09 -0300

    Estacionamento.java

--- codigo/Estacionamento.java
+++ codigo/Estacionamento.java
@@ -1,51 +1,75 @@
-
-
+// A classe Estacionamento representa um estacionamento com vagas e clientes.
 public class Estacionamento {
 
-	private String nome;
-	private Cliente[] id;
-	private Vaga[] vagas;
-	private int quantFileiras;
-	private int vagasPorFileira;
+    // Nome do estacionamento.
+    private String nome;
 
-	public Estacionamento(String nome, int fileiras, int vagasPorFila) {
-		
-	}
+    // Array de clientes no estacionamento.
+    private Cliente[] clientes;
 
-	public void addVeiculo(Veiculo veiculo, String idCli) {
-		
-	}
+    // Array de vagas no estacionamento.
+    private Vaga[] vagas;
 
-	public void addCliente(Cliente cliente) {
-		
-	}
+    // Número de fileiras no estacionamento.
+    private int quantFileiras;
 
-	private void gerarVagas() {
-		
-	}
+    // Número de vagas por fileira no estacionamento.
+    private int vagasPorFileira;
 
-	public void estacionar(String placa) {
-		
-	}
+    // Construtor da classe Estacionamento.
+    public Estacionamento(String nome, int fileiras, int vagasPorFila) {
+        this.nome = nome;
+        this.quantFileiras = fileiras;
+        this.vagasPorFileira = vagasPorFila;
+        // Inicializa arrays de clientes e vagas.
+        this.clientes = new Cliente[0]; // Array vazio inicialmente.
+        this.vagas = new Vaga[0]; // Array vazio inicialmente.
+        // Gera as vagas do estacionamento.
+        gerarVagas();
+    }
 
-	public double sair(String placa) {
-		
-	}
+    // Adiciona um veículo ao estacionamento associando-o a um cliente.
+    public void addVeiculo(Veiculo veiculo, String idCli) {
+        // Implementação do método.
+    }
 
-	public double totalArrecadado() {
-		
-	}
+    // Adiciona um cliente ao estacionamento.
+    public void addCliente(Cliente cliente) {
+        // Implementação do método.
+    }
 
-	public double arrecadacaoNoMes(int mes) {
-		
-	}
+    // Gera as vagas do estacionamento com base no número de fileiras e vagas por fileira.
+    private void gerarVagas() {
+        // Implementação do método.
+    }
 
-	public double valorMedioPorUso() {
-		
-	}
+    // Estaciona um veículo no estacionamento com base na placa.
+    public void estacionar(String placa) {
+        // Implementação do método.
+    }
 
-	public String top5Clientes(int mes) {
-		
-	}
+    // Remove um veículo do estacionamento com base na placa e calcula o valor a ser pago.
+    public double sair(String placa) {
+        // Implementação do método.
+    }
 
+    // Calcula e retorna o total arrecadado pelo estacionamento.
+    public double totalArrecadado() {
+        // Implementação do método.
+    }
+
+    // Calcula e retorna a arrecadação no mês especificado.
+    public double arrecadacaoNoMes(int mes) {
+        // Implementação do método.
+    }
+
+    // Calcula e retorna o valor médio por uso do estacionamento.
+    public double valorMedioPorUso() {
+        // Implementação do método.
+    }
+
+    // Retorna os top 5 clientes do mês especificado.
+    public String top5Clientes(int mes) {
+        // Implementação do método.
+    }
 }
