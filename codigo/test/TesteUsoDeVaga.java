eb14494e7f75346e6ab3fb69c8fc012832b1b336
Wanessa Dias <130778028+diascw07@users.noreply.github.com>
2023-11-22 10:59:30 -0300

    TesteUsoDeVaga.java

--- codigo/test/TesteUsoDeVaga.java
+++ codigo/test/TesteUsoDeVaga.java
@@ -3,18 +3,23 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-
+// A classe de teste TesteUsoDeVaga testa os métodos da classe UsoDeVaga.
 public class TesteUsoDeVaga {
 
+    // Instância de UsoDeVaga a ser testada.
     private UsoDeVaga usoDeVaga;
+
+    // Instância de Vaga associada ao uso.
     private Vaga vaga;
 
+    // Método executado antes de cada teste para configurar o ambiente de teste.
     @Before
     public void setUp() {
         vaga = new Vaga(1, "A");
         usoDeVaga = new UsoDeVaga(vaga);
     }
 
+    // Testa o método usarVaga, verificando se a entrada é registrada corretamente.
     @Test
     public void testUsarVaga() {
         LocalDateTime entrada = LocalDateTime.of(2023, 10, 20, 12, 0);
@@ -22,6 +27,7 @@ public class TesteUsoDeVaga {
         assertEquals(entrada, usoDeVaga.getEntrada());
     }
 
+    // Testa o método sair, verificando se a saída é registrada e o valor pago é não nulo.
     @Test
     public void testSair() {
         LocalDateTime entrada = LocalDateTime.of(2023, 10, 20, 12, 0);
@@ -33,6 +39,7 @@ public class TesteUsoDeVaga {
         assertNotNull(usoDeVaga.valorPago());
     }
 
+    // Testa o método valorPago, verificando se o valor pago é calculado corretamente.
     @Test
     public void testValorPago() {
         LocalDateTime entrada = LocalDateTime.of(2023, 10, 20, 12, 0);
