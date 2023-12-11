27c7bb66907eaaf26a3d8ed180d479e5e493c4c6
Paula de Freitas <113747605+pauladefreitas@users.noreply.github.com>
2023-12-11 19:44:14 -0300

    delete nao usado

--- codigo/src/Estacionamento/IEstacionamento.java
+++
@@ -1,6 +0,0 @@
-import java.time.LocalDateTime;
-
-public interface IEstacionamento {
-    public double sair(Vaga vaga, LocalDateTime saida);
-    public void estacionar(Vaga vaga, LocalDateTime entrada);
-}
