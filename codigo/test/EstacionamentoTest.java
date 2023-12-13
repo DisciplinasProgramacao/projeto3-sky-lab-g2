dacbd0e8af5262aeb0c554557d004329e70edf18
Wanessa Dias <130778028+diascw07@users.noreply.github.com>
2023-12-12 22:55:09 -0300

    EstacionamentoTest.java

--- codigo/test/EstacionamentoTest.java
+++ codigo/test/EstacionamentoTest.java
@@ -1,56 +1,43 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class EstacionamentoTest {
 
+    private Estacionamento estacionamento;
+    private Cliente cliente;
+    private Veiculo veiculo;
+
+    @Before
+    public void setUp() {
+        estacionamento = new Estacionamento("Estacionamento Teste", 3, 5);
+        cliente = new Cliente("1", "Cliente Teste");
+        veiculo = new Veiculo("ABC123");
+    }
+
     @Test
     public void testAdicionarCliente() throws ClienteJaExistenteException {
-        Estacionamento estacionamento = new Estacionamento("Estacionamento Teste", 3, 5);
-        Cliente cliente = new Cliente("1", "Cliente Teste");
-
-        // Adicionar cliente
         estacionamento.addCliente(cliente);
-
-        // Verificar se o cliente foi adicionado corretamente
         assertTrue(estacionamento.getClientes().contains(cliente));
     }
 
     @Test(expected = ClienteJaExistenteException.class)
     public void testAdicionarClienteExistente() throws ClienteJaExistenteException {
-        Estacionamento estacionamento = new Estacionamento("Estacionamento Teste", 3, 5);
-        Cliente cliente = new Cliente("1", "Cliente Teste");
-
-        // Adicionar cliente pela primeira vez
         estacionamento.addCliente(cliente);
-
-        // Tentar adicionar o mesmo cliente novamente, deve lançar exceção
-        estacionamento.addCliente(cliente);
+        estacionamento.addCliente(cliente); // Deve lançar exceção
     }
 
     @Test
-    public void testAdicionarVeiculo() throws VeiculoJaExistenteException, ClienteNaoExisteException {
-        Estacionamento estacionamento = new Estacionamento("Estacionamento Teste", 3, 5);
-        Cliente cliente = new Cliente("1", "Cliente Teste");
-        try {
-            estacionamento.addCliente(cliente);
-        } catch (ClienteJaExistenteException e) {
-            e.printStackTrace();
-        }
-
-        Veiculo veiculo = new Veiculo("ABC123");
-
+    public void testAdicionarVeiculo() throws VeiculoJaExistenteException, ClienteNaoExisteException, ClienteJaExistenteException {
+        estacionamento.addCliente(cliente);
         estacionamento.addVeiculo(veiculo, cliente.getId());
-
-        assertTrue(cliente.possuiVeiculo("ABC123") != null);
+        assertTrue(cliente.possuiVeiculo(veiculo.getPlaca()) != null);
     }
 
     @Test(expected = ClienteNaoExisteException.class)
     public void testAdicionarVeiculoClienteNaoExistente() throws VeiculoJaExistenteException, ClienteNaoExisteException {
-        Estacionamento estacionamento = new Estacionamento("Estacionamento Teste", 3, 5);
-        Veiculo veiculo = new Veiculo("ABC123");
-
         estacionamento.addVeiculo(veiculo, "ClienteInexistente");
     }
 
-
+    // Aqui você pode adicionar mais testes para cobrir outros aspectos da classe Estacionamento
 }
